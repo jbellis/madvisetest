b4e9290e0b6a32ff0e021ab4d08dce44b70528f6
Jonathan Ellis <jbellis@datastax.com>
2024-07-02 09:45:26 -0500

    2GB regions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,12 +8,15 @@ import java.io.IOException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Main {
 
     private static final String FILENAME = "/home/ubuntu/cassandra/data/data/coherebench/embeddings_table-a5b9e560347b11efaf001bb676958899/cc-3ghc_1q72_2xo0g2nt06q1h93ee1-bti-SAI+ca+embeddings_table_embedding_idx+TermsData.db";
     private static final int READ_SIZE = 10;
+    private static final long MAX_REGION_SIZE = Integer.MAX_VALUE;
 
     private interface CLibrary extends com.sun.jna.Library {
         int madvise(Pointer addr, long length, int advice);
@@ -28,30 +31,49 @@ public class Main {
             System.out.println("File Size: " + fileSize); // Debug print
 
             try (FileChannel fileChannel = FileChannel.open(file.toPath(), StandardOpenOption.READ)) {
-                MappedByteBuffer fileMap = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileSize);
-
-                // Apply MADV_RANDOM to the memory-mapped region
-                int result = CLibrary.INSTANCE.madvise(Native.getDirectBufferPointer(fileMap), fileSize, 1 /* MADV_RANDOM */);
-                if (result != 0) {
-                    throw new IOException("Error applying MADV_RANDOM: " + result);
-                }
-
-                readRandomBytes(fileMap, fileSize);
+                List<MappedByteBuffer> regions = mapFileRegions(fileChannel, fileSize);
+                readRandomBytes(regions, fileSize);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void readRandomBytes(MappedByteBuffer fileMap, long fileSize) {
+    private static List<MappedByteBuffer> mapFileRegions(FileChannel fileChannel, long fileSize) throws IOException {
+        List<MappedByteBuffer> regions = new ArrayList<>();
+        long position = 0;
+
+        while (position < fileSize) {
+            long regionSize = Math.min(MAX_REGION_SIZE, fileSize - position);
+            MappedByteBuffer fileMap = fileChannel.map(FileChannel.MapMode.READ_ONLY, position, regionSize);
+
+            // Apply MADV_RANDOM to the memory-mapped region
+            int result = CLibrary.INSTANCE.madvise(Native.getDirectBufferPointer(fileMap), regionSize, 1 /* MADV_RANDOM */);
+            if (result != 0) {
+                throw new IOException("Error applying MADV_RANDOM: " + result);
+            }
+
+            regions.add(fileMap);
+            position += regionSize;
+        }
+
+        return regions;
+    }
+
+    private static void readRandomBytes(List<MappedByteBuffer> regions, long fileSize) {
         Random random = new Random();
         int checksum = 0;
 
         byte[] buffer = new byte[READ_SIZE];
-        var forever = true;
+        boolean forever = true;
+
         while (forever) {
             long randomPosition = generateRandomPosition(fileSize - READ_SIZE + 1, random);
-            fileMap.position((int) randomPosition);
+            int regionIndex = (int) (randomPosition / MAX_REGION_SIZE);
+            long regionOffset = randomPosition % MAX_REGION_SIZE;
+
+            MappedByteBuffer fileMap = regions.get(regionIndex);
+            fileMap.position((int) regionOffset);
             fileMap.get(buffer, 0, READ_SIZE);
 
             for (int i = 0; i < READ_SIZE; i++) {
